82406ae149cd2e9a1bcdfc94b5d51afea87bca53
mdemon420 <mdeamon4@gmail.com>
2022-06-29 00:38:02 +0600

    logging user show display

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialize';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
 
 initializeAuthentication()
 const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({})
   const handleGoogleSignIn = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
+        const { displayName, photoURL, email } = result.user;
         console.log(user)
+        const loggedInUser = {
+          name: displayName,
+          photo: photoURL,
+          email: email
+        };
+        setUser(loggedInUser);
       })
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email && <div>
+          <h1>Welcome  {user.name}</h1>
+          <h3>I know your email {user.email}</h3>
+          <img src={user.photo} alt=''></img>
+        </div>
+      }
     </div>
   );
 }
