8f32aaef8924ca9018871e7b3937ec5b3b47333d
mdemon420 <mdeamon4@gmail.com>
2022-06-28 20:53:54 +0600

    add firebase config and initializeapp

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1yuo8ucPMNhD7TaHrMQoz3QZUs33W-8M",
+    authDomain: "simple-firebase-authenti-93a79.firebaseapp.com",
+    projectId: "simple-firebase-authenti-93a79",
+    storageBucket: "simple-firebase-authenti-93a79.appspot.com",
+    messagingSenderId: "314300614848",
+    appId: "1:314300614848:web:c1f2f36ab7fd83490fc525"
+};
+
+export default firebaseConfig
